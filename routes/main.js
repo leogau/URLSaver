8ced430a2ed84797fdf25510d4f2a3aee0567c5d
Leo Gau <leo.gau1@gmail.com>
2012-06-24 23:12:42 -0700

    Add ability to display links when visiting /u/

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,43 @@
-var utils = require('../lib/utils');
+var utils = require('../lib/utils'),
+	db = app.db;
+
+/*
+ * Convert milliseconds to correct dates
+ * an return as an object.
+ *
+ * Expecting array with format: [url, int, ...]
+ */
+var dateConvert = function(arr) {
+	var obj = {};
+	arr.forEach(function(elem, idx, array) {
+		if (idx % 2 === 1) {
+			var d = new Date(parseInt(elem, 10));
+			obj[d.toDateString()] = array[idx-1];
+		}
+	});
+	console.log(obj);
+	return obj;
+};
 
 /*
  * GET home page
  */
 app.get('/', function(req, res, next) {
 	res.render('index');
+});
+
+/*
+ * GET all urls
+ */
+app.get('/u', function(req, res, next) {
+	var from = 0,
+		to = -1;
+
+	db.zrange('urlsaver:urls', from, to, "withscores", function(err, reply) {
+		if (err) return next(err);
+		
+		res.render('urls', { urls: dateConvert(reply) });
+	});
 });
 
 /*
