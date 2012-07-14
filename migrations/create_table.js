02749d03a924ec7fc91d20328cec58fe5fa33b38
Leo Gau <leo.gau1@gmail.com>
2012-07-14 15:38:02 -0700

    db debugging

--- migrations/create_table.js
+++ migrations/create_table.js
@@ -3,12 +3,14 @@ var pg = require('pg').native,
     client,
     query;
 
-var psql = 'CREATE TABLE urls (  urls_id serial NOT NULL,  time_created timestamp with time zone NOT NULL,  url text NOT NULL,  CONSTRAINT "Primary Key" PRIMARY KEY (urls_id))';
+var psql = 'CREATE TABLE urls (urls_id serial NOT NULL, time_created timestamp with time zone NOT NULL, url text NOT NULL, CONSTRAINT "Primary Key" PRIMARY KEY (urls_id))';
 
+console.log("c string: ", connectionString);
 console.log("about to create client");
 client = new pg.Client(connectionString);
 console.log("about to connect to db");
 client.connect();
 console.log("client connected");
 query = client.query(psql);
-query.on('end', function() { client.end(); });
+console.log("query executed");
+query.on('end', function() { console.log("end recieved"); client.end(); });
